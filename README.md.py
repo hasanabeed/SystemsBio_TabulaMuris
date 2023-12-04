7c9fd9a793db3c90141f218e0b58e768236e603f
Md Abeed Hasan <60261814+hasanabeed@users.noreply.github.com>
2023-12-04 23:03:11 +0100

    Add files via upload
    add readme file

---
+++ README.md.py
@@ -0,0 +1,96 @@
+# Tabula Muris: A Comprehensive Mouse Atlas
+
+## Table of Contents
+
+- [About the Project](#about-project)
+  - [Built With](#built-with)
+  - [Key Features](#key-features)
+- [Getting Started](#getting-started)
+  - [Setup](#setup)
+  - [Prerequisites](#prerequisites)
+  - [Install](#install)
+- [Students](#students)
+- [Contributing](#contributing)
+- [Future Features](#future-features)
+- [Show your support](#support)
+- [Acknowledgements](#acknowledgements)
+- [Project Name](#project-name)
+  - [About this project](#about-this-project)
+
+## About the Project <a name="about-project"></a>
+
+The **Tabula Muris** project is a comprehensive single-cell transcriptomic atlas of *Mus musculus* (mouse), covering data from over 100,000 cells across 20 different organs. The primary goal is to offer a valuable resource for cell biology and provide insights into poorly characterized cell populations. Employing advanced techniques such as microfluidic droplet-based counting and full-length transcript analysis, the project goes beyond traditional cell characterization methods.
+
+### Built With <a name="built-with"></a>
+
+- Python
+- Jupyter Notebooks
+- Other dependencies (please refer to the [Getting Started](#getting-started) section for details)
+
+#### Key Features <a name="key-features"></a>
+
+- **Mouse Atlas Creation:** Comprehensive analysis of single-cell transcriptomic data from various organs.
+- **Cell Type Characterization:** Definition of cell types using advanced methods, revealing insights into poorly characterized populations.
+- **Methodological Comparison:** Comparison of two distinct single-cell RNA-sequencing methods (FACS and microfluidic-droplet) to understand technical biases and differences.
+- **Global Clustering:** Exploration of relationships between cells from different organs to identify shared and distinct cell types.
+
+## Getting Started <a name="getting-started"></a>
+
+### Setup <a name="setup"></a>
+
+To get this project up and running, follow these simple steps.
+
+### Prerequisites <a name="prerequisites"></a>
+
+Make sure you have the following installed:
+
+- Python
+- Jupyter Notebooks
+
+### Install <a name="install"></a>
+
+1. Clone the repository.
+   ```sh
+   git clone https://github.com/hasanabeed/SystemsBio_TabulaMuris.git
+
+
+## 👨‍🎓👩‍🎓  Students <a name="acknowledgements"></a>
+- [Md Abeed Hasan](#https://www.linkedin.com/in/abeed-hasan-a09469119/)
+- [Lubna Rahman](#https://www.linkedin.com/in/lubna-rahman-219ba1175/)
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions are what make the open-source community such an amazing place to be, learn, and inspire. Any contributions you make are greatly appreciated.
+
+- Fork the Project
+- Create your Feature Branch (git checkout -b feature/AmazingFeature)
+- Commit your Changes (git commit -m 'Add some AmazingFeature')
+- Push to the Branch (git push origin feature/AmazingFeature)
+- Open a Pull Request
+
+## 🔭 Future Features <a name="future-features"></a>
+- Expand the dataset to include more cell types, organs, and disease models.
+- Integrate Tabula Muris data with other datasets (spatial transcriptomics, single-cell epigenomics, and proteomics).
+- Develop new computational tools and methods for analysis and visualization.
+- Apply Tabula Muris data to solve specific biological and medical problems.
+
+## ⭐ Show your support <a name="support"></a>
+Give a ⭐️ if you found this project interesting!
+
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+We would like to convey our sincere gratitude to our:
+
+Instructors:
+- [Gautier Stoll](https://www.linkedin.com/in/gautier-stoll-b9495a/?originalSubdomain=fr)
+- [Laurence Calzone](https://www.linkedin.com/in/laurence-calzone-66848a14/?originalSubdomain=fr)
+- [Loredana Martignetti](https://scholar.google.com/citations?user=uIq1vX4AAAAJ&hl=fr)
+- [Wolfram Liebermeiste](https://www.linkedin.com/in/wolfram-liebermeister-28264226/?originalSubdomain=fr) 
+
+Institutions:
+
+- [Learning Planet Institute](https://www.learningplanetinstitute.org/en/)
+- [Université Paris Cité](https://u-paris.fr/en/)
